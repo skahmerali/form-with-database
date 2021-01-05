e3591c69c5a4e092484da8809f1025632bf40140
skahmerali <38328235+skahmerali@users.noreply.github.com>
2021-01-05 17:57:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function abc() {
+
+    var name = document.getElementById("name").Value;
+    var email = document.getElementById("email").Value;
+    var password = document.getElementById("password").Value;
+    var number = document.getElementById("number").Value;
+
+}
